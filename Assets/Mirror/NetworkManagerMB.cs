e63529a04864f1ed2d6e7d848e6212e00f91fc54
Lsday <rondaswim@hotmail.com>
2021-05-20 19:13:04 +0200

    Initial unity project

---
+++ Assets/Mirror/NetworkManagerMB.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Mirror;
+
+/*
+	Documentation: https://mirror-networking.gitbook.io/docs/components/network-manager
+	API Reference: https://mirror-networking.com/docs/api/Mirror.NetworkManager.html
+*/
+
+public class NetworkManagerMB : NetworkManager
+{
+    
+
+
+   
+    
+
+   
+}

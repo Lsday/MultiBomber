bc5d9269c5c756dcde5069e108b41d9ed60d03d0
Alexandre Labedade <alesk@alesk.fr>
2021-05-27 23:56:57 +0200

    Some comments

--- Assets/Scripts/Player/PlayerEntity.cs
+++ Assets/Scripts/Player/PlayerEntity.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 using Mirror;
 public class PlayerEntity : NetworkBehaviour
 {
+    #region Static declarations
     public static List<PlayerEntity> instancesList = new List<PlayerEntity>();
     public static void SortInstances()
     {
+        // TODO : add a "creation timestamp" to sort the players with it, instead of hubidentity+localindex
         instancesList.Sort((x, y) => {
             var ret = x.hubIdentity.netId.CompareTo(y.hubIdentity.netId);
             if (ret == 0) ret = x.localPlayerIndex.CompareTo(y.localPlayerIndex);
@@ -14,25 +16,34 @@ public class PlayerEntity : NetworkBehaviour
         });
     }
 
+    /// <summary>
+    /// This function returns the number of all players (local+remote)
+    /// </summary>
+    /// <returns></returns>
     public static int GetInstancesCount()
     {
         return instancesList.Count;
     }
+    #endregion
 
-    [SyncVar] public string debugName;
+    #region Properties
+    [SyncVar] public string debugName; // TODO : remove this later
     [SyncVar] public NetworkIdentity hubIdentity;
     [SyncVar] public byte localPlayerIndex;
     [SyncVar] public Color defaultColor;
 
-    //public byte playerIndex;
-
+    /// <summary>
+    /// The device (physical=keyboard,gamepad or virtual=bot) used to control this player
+    /// </summary>
     public DeviceEntity controllerDevice;
+
+    // TODO : private variables
     public Animator animator;
     public Renderer avatarRenderer;
     public Material avatarMaterial;
+    #endregion
 
-
-
+    #region Init
     private void Awake()
     {
         animator = GetComponentInChildren<Animator>();
@@ -40,6 +51,41 @@ public class PlayerEntity : NetworkBehaviour
         avatarMaterial = avatarRenderer.material;
     }
 
+    /// <summary>
+    /// Pointer to the NetworkIndentity of the PlayerHub which created this player.
+    /// </summary>
+    /// <param name="identity"></param>
+    public void SetHubIdentity(NetworkIdentity identity)
+    {
+        hubIdentity = identity;
+    }
+
+    /// <summary>
+    /// Index of this player on its local machine
+    /// </summary>
+    /// <param name="index"></param>
+    public void SetLocalPlayerIndex(byte index)
+    {
+        localPlayerIndex = index;
+    }
+
+    private void OnEnable()
+    {
+        // Add this Player to the local instances list, to keep track of all the available players
+        instancesList.Add(this);
+
+        // Sort the instances list, to keep the same order on all clients and the server
+        if (hubIdentity) SortInstances();
+    }
+
+    private void OnDisable()
+    {
+        instancesList.Remove(this);
+        if (hubIdentity) SortInstances();
+    }
+    #endregion
+
+    #region Local functions
     private void Start()
     {
 
@@ -59,41 +105,21 @@ public class PlayerEntity : NetworkBehaviour
         }
 
         ApplyColor();
-
-
-    }
-
-    [ClientRpc]
-    void RpcSetColor(Color newColor)
-    {
-        defaultColor = newColor;
-        ApplyColor();
     }
 
     void ApplyColor()
     {
         avatarMaterial.color = defaultColor;
     }
+    #endregion
 
-    public void SetHubIdentity(NetworkIdentity identity)
-    {
-        hubIdentity = identity;
-    }
 
-    public void SetLocalPlayerIndex(byte index)
+    #region Network functions
+    [ClientRpc]
+    void RpcSetColor(Color newColor)
     {
-        localPlayerIndex = index;
+        defaultColor = newColor;
+        ApplyColor();
     }
-
-    private void OnEnable()
-    {
-        instancesList.Add(this);
-        if(hubIdentity) SortInstances();
-    }
-
-    private void OnDisable()
-    {
-        instancesList.Remove(this);
-        if (hubIdentity) SortInstances();
-    }
+    #endregion
 }
--- Assets/Scripts/Player/PlayersHub.cs
+++ Assets/Scripts/Player/PlayersHub.cs
@@ -4,32 +4,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This class is instanciated for each client
+/// it is managing all the players physically playing through this client
+/// </summary>
 public class PlayersHub : NetworkBehaviour
 {
+    #region Static declarations
     public static List<PlayersHub> instancesList = new List<PlayersHub>();
 
     public static PlayersHub instance;
+    #endregion
 
+    #region Properties
     public GameObject playerPrefab;
     public SO_Int localPlayersCount;
     public SO_Int totalPlayersCount;
 
+    // TODO : private variable
     public List<PlayerEntity> players = new List<PlayerEntity>();
+    #endregion
 
+    #region Init
     private void Start()
     {
         transform.position = Vector3.zero;
 
         if (isLocalPlayer)
         {
+            // identify this script as the local instance of this class
             instance = this;
+
+            // create a Player for every connected input device (keyboard, gamepad)
             CreatePlayers();
         }
     }
-    
+    #endregion
+
+    #region Local functions
     void CreatePlayers()
     {
-        for(byte i = 0; i < localPlayersCount.value; i++)
+        //localPlayersCount is updated to reflect all the connected input devices and bots initialized on this client
+        for (byte i = 0; i < localPlayersCount.value; i++)
         {
             AddPlayer(i); 
         }
@@ -42,15 +58,26 @@ public class PlayersHub : NetworkBehaviour
             CmdSpawnPlayer(netIdentity, localPlayerIndex);
         }
     }
+    #endregion
 
+    #region Network functions
+
+
+    // TODO : this function create an avatar for the main game
+    // it should also initialize another class to manage the options for this player setup menu
     [Command]
     private void CmdSpawnPlayer(NetworkIdentity identity , byte localPlayerIndex)
     {
+        // instantiate de Player prefab
         GameObject playerGameObject = Instantiate(playerPrefab, transform.position, Quaternion.identity);
 
+        // Get the Player Entity of this instance
         PlayerEntity playerEntity = playerGameObject.GetComponent<PlayerEntity>();
+        
+        // parent it to the hub for a better clarity in the hierarchy
         playerEntity.transform.parent = transform;
 
+        // 
         playerEntity.SetHubIdentity(identity);
         playerEntity.SetLocalPlayerIndex(localPlayerIndex);
 
@@ -68,4 +95,6 @@ public class PlayersHub : NetworkBehaviour
     {
         totalPlayersCount.value = PlayerEntity.GetInstancesCount();
     }
+    #endregion
+
 }

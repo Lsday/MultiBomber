0a8855600cbda08e36664ffe0b1ee0fb047253ae
Alexandre Labedade <alesk@alesk.fr>
2021-06-02 23:58:05 +0200

    Compute bonuses repartition

---
+++ Assets/Scripts/Editor/SO_LevelBonusSettings_Inspector.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+[CustomEditor(typeof(SO_LevelBonusSettings)), CanEditMultipleObjects]
+
+public class SO_LevelBonusSettings_Inspector : Editor
+{
+    SO_LevelBonusSettings myTarget;
+
+    SerializedProperty bonusList;
+    SerializedObject GetTarget;
+    int ListSize;
+
+    private void OnEnable()
+    {
+        myTarget = (SO_LevelBonusSettings)target;
+        bonusList = this.serializedObject.FindProperty("_bonusSettings");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        // https://forum.unity.com/threads/display-a-list-class-with-a-custom-editor-script.227847/
+        //EditorGUILayout.PropertyField(bonusList, true,null);
+
+        SO_LevelBonusSettings.BonusStock[] results =  myTarget.ComputeBonusList();
+
+        int total = Mathf.CeilToInt(myTarget.boxesCount * myTarget.bonusPercentage);
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("Total: "+total.ToString(), GUILayout.Width(100));
+        EditorGUILayout.Space();
+        int sum = 0;
+
+        for (int i=0;i< results.Length; i++)
+        {
+            var dataRef = results[i];
+            if(dataRef.count > 0)
+            {
+                sum += dataRef.count;
+                EditorGUILayout.LabelField(dataRef.name + " " + dataRef.count.ToString(), GUILayout.Width(100));
+            }
+            
+        }
+        EditorGUILayout.Space();
+        EditorGUILayout.LabelField("Sum: " + sum.ToString(), GUILayout.Width(100));
+
+        if (GUI.changed) { EditorUtility.SetDirty(myTarget); }
+    }
+}
---
+++ Assets/Scripts/Utils/ScriptableObjects/SO_LevelBonusSettings.cs
@@ -0,0 +1,124 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LevelBonusSettings", menuName = "SO Variable/Level Bonus Settings", order = 1)]
+public class SO_LevelBonusSettings : ScriptableObject
+{
+
+    public struct BonusStock
+    {
+        public int count;
+        public string name; // TODO : remplacer par le pointeur vers le scriptableobject du bonus
+    }
+
+
+    [System.Serializable]
+    public struct BonusDropSettings
+    {
+        public string name;
+
+        [Range(0,10)]
+        public int exactCount;
+
+        [Range(0f,1f)]
+        public float proportion;
+
+        [Range(0f, 5f)]
+        public float playersProportion;
+
+        public BonusDropSettings(string name ,int exactCount , float proportion, float playersProportion)
+        {
+            this.name = name;
+            this.exactCount = exactCount;
+            this.proportion = proportion;
+            this.playersProportion = playersProportion;
+        }
+    }
+
+    public List<BonusDropSettings> bonusSettings
+    {
+        get
+        {
+            return _bonusSettings;
+        }
+    }
+
+    [SerializeField]
+    private List<BonusDropSettings> _bonusSettings = new List<BonusDropSettings>();
+
+    [Range(0,500)]
+    public int boxesCount = 100;
+
+    [Range(1,10)]
+    public int playersCount = 1;
+
+    [Range(0f, 1f)]
+    public float bonusPercentage = 1f;
+
+    void OnEnable()
+    {
+        // Awake
+    }
+
+    public BonusStock[] ComputeBonusList()
+    {
+        int total = Mathf.CeilToInt(boxesCount * bonusPercentage);
+
+        BonusStock[] result = new BonusStock[_bonusSettings.Count];
+
+        // get exact counts
+        int totalExact = 0;
+        for (int i = 0; i < _bonusSettings.Count; i++)
+        {
+            BonusDropSettings settings = _bonusSettings[i];
+            result[i].name = settings.name;
+
+            if (settings.exactCount > 0)
+            {
+                result[i].count = Mathf.CeilToInt(settings.exactCount * (settings.playersProportion > 0 ? settings.playersProportion * playersCount : 1));
+                totalExact += result[i].count;
+            }
+        }
+
+        if(totalExact > total)
+        {
+            int newtotal = 0;
+            for (int i = 0; i < _bonusSettings.Count; i++)
+            {
+                if (result[i].count > 0)
+                {
+                    result[i].count = Mathf.CeilToInt(((float)result[i].count/totalExact) * total);
+                    newtotal += result[i].count;
+                }
+            }
+            totalExact = newtotal;
+        }
+
+        int totalProportionnal = total - totalExact;
+        if(totalProportionnal > 0)
+        {
+            float floatTotal = 0;
+            float[] proportions = new float[_bonusSettings.Count];
+            for (int i = 0; i < _bonusSettings.Count; i++)
+            {
+                BonusDropSettings settings = _bonusSettings[i];
+                if (settings.exactCount == 0 && settings.proportion > 0)
+                {
+                    proportions[i] = (settings.proportion * totalProportionnal * (settings.playersProportion > 0 ? settings.playersProportion * playersCount : 1));
+                    floatTotal += proportions[i];
+                }
+            }
+
+            for (int i = 0; i < _bonusSettings.Count; i++)
+            {
+                BonusDropSettings settings = _bonusSettings[i];
+                if (settings.exactCount == 0 && settings.proportion > 0)
+                {
+                    result[i].count = Mathf.RoundToInt((proportions[i] / floatTotal) * totalProportionnal);
+                }
+            }
+        }
+        return result;
+    }
+}
